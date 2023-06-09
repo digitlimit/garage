24ff3547cb5c863de4cb045cbe5ff1401a6ce6a1
Emeka Mbah <frankemeks77@yahoo.com>
2023-06-09 18:17:19 +0100

    create models, migrations, tests, controllers

---
+++ app/Http/Controllers/ClosedSlotController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreClosedSlotRequest;
+use App\Http\Requests\UpdateClosedSlotRequest;
+use App\Models\ClosedSlot;
+
+class ClosedSlotController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreClosedSlotRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ClosedSlot $closedSlot)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(ClosedSlot $closedSlot)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateClosedSlotRequest $request, ClosedSlot $closedSlot)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ClosedSlot $closedSlot)
+    {
+        //
+    }
+}
