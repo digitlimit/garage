bb145f3372ace65de1847795d607c46c559c498e
Emeka Mbah <frankemeks77@yahoo.com>
2023-06-10 13:19:36 +0100

    create tests

--- tests/Feature/API/BookingTest.php
+++ tests/Feature/API/BookingTest.php
@@ -1,25 +1,64 @@
 <?php
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Laravel\Sanctum\Sanctum;
 use App\Models\Booking;
-use Carbon\Carbon;
+use App\Models\User;
 
 uses(RefreshDatabase::class);
-beforeEach(fn () => $this->seed(DatabaseSeeder::class));
 
-it('can fetch upcoming bookings', function () 
+it('can create new bookings', function () 
 {
+    $response = $this
+        ->postJson("/api/bookings",[
+            'name'  => 'Emeka',
+            'email' => 'frankemeks77@yahoo.com'
+        ])
+        ->assertOk();
+
+    $this->assertDatabaseMissing('bookings', ['client_id' => 1]);
+
+})->group('api', 'api-booking', 'api-booking-create-new');
+
+
+it('can only allow 30 minutes booking intervals', function () 
+{
+
+})->group('api', 'api-booking', 'api-booking-interval');
+
+it('can only allow bookings within the opening time', function () 
+{
+
+})->group('api', 'api-booking', 'api-booking-opening-time');
+
+it('can only allow 1 bookings per slot', function () 
+{
+
+})->group('api', 'api-booking', 'api-booking-per-slot');
+
+it('can only allow admin to fetch a list of upcoming bookings', function () 
+{
+    $paul     = User::factory()->create(['name' => 'Paul', 'admin' => true]);
     $booking  = Booking::factory()->create();
-    $response = $this->getJson("/api/bookings");
 
-    $data = [
-        'message' => 'Retrieved To-do',
-        'booking' => [
-            'id' => $booking->id,
-            'name' => $booking->name,
-            'completed' => $booking->completed,
-        ]
-    ];
+    Sanctum::actingAs($paul);
 
-    $response->assertStatus(200)->assertJson($data);
-})->group('api', 'booking-api');
+    $response = $this->getJson("/api/bookings/upcoming")->assertOk();
+    $content  = $response->decodeResponseJson()['data'];
+
+    expect($content)
+        ->toMatchArray(['id' => $booking->id]);
+
+})->group('api', 'api-booking', 'api-booking-upcoming');
+
+it('can filter bookings to a specific day', function () 
+{
+   
+
+})->group('api', 'api-booking', 'api-booking-filter');
+
+it('can send bookings confirmation email to admin and customer', function () 
+{
+   
+
+})->group('api', 'api-booking', 'api-booking-emails');
