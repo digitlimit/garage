e887b7a6a65d2442960abcde7951248dd6f26857
Emeka Mbah <frankemeks77@yahoo.com>
2023-06-10 16:40:26 +0100

    setup routes

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -2,11 +2,36 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\StoreBookingRequest;
-use App\Http\Requests\UpdateBookingRequest;
-use App\Models\Booking;
+use App\Http\Requests\Booking\CreateRequest;
+use App\Http\Requests\Booking\ListRequest;
 
 class BookingController extends Controller
 {
-    
+    /**
+     * Fetch a listing a bookings.
+     */
+    public function list(ListRequest $request)
+    {
+      
+    }
+
+    /**
+     * Store a newly created booking in storage.
+     */
+    public function create(CreateRequest $request)
+    {
+        // try {
+        //     // only validated form inputs
+        //     $name  = $request->validated('name');
+        //     $email = $request->validated('email');
+        //     $phone = $request->validated('phone');
+        //     $model = $request->validated('model');
+        //     $make  = $request->validated('make');
+        //     $start = $request->date('slot_start');
+        //     $end   = $request->date('slot_end');
+
+        // } catch(\Exception $e) {
+        //     // return json 
+        // }
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\BookingController;
+use App\Http\Controllers\SlotController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +17,35 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('/auth/login', [AuthController::class, 'login'])
+->name('auth.login');
+
+Route::post('/bookings',   [BookingController::class, 'create'])
+->name('bookings.create');
+
+Route::middleware('auth:sanctum')
+->group(function () 
+{
+    Route::controller(AuthController::class)
+    ->prefix('auth')
+    ->name('auth.')
+    ->group(function () {
+        Route::post('/logout', 'logout')->name('logout');
+    });
+
+    Route::controller(BookingController::class)
+    ->prefix('bookings')
+    ->name('bookings.')
+    ->group(function () {
+        Route::get('/',  'list')->name('list')->can('list', 'booking');
+    });
+
+    Route::controller(SlotController::class)
+    ->prefix('slots')
+    ->name('slots.')
+    ->group(function () {
+        Route::post('/close', 'close')->name('close')->can('close', 'slot');
+        Route::post('/open',  'open')->name('open')->can('open', 'slot');
+    });
+
 });
