117105f91911cb6a991bec8c2de2e1a994a39e63
Emeka Mbah <frankemeks77@yahoo.com>
2023-06-10 17:16:24 +0100

    bind respositories in service provider

---
+++ app/Providers/RepositoryServiceProvider.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class RepositoryServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        $this->app->bind(
+            \App\Repositories\Contracts\BookingRepository::class, 
+            \App\Repositories\Eloquent\BookingRepository::class
+        );
+
+        $this->app->bind(
+            \App\Repositories\Contracts\ClientRepository::class, 
+            \App\Repositories\Eloquent\ClientRepository::class
+        );
+
+        $this->app->bind(
+            \App\Repositories\Contracts\VehicleRepository::class, 
+            \App\Repositories\Eloquent\VehicleRepository::class
+        );
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+}
